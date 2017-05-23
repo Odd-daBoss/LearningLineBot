e61457619c21e660aa9b6389db7aa5dee34a34b8
DESKTOP-2JD4TJA\SOLUTION <odd.daboss@sdee.co>
2017-05-23 11:16:33 +0700

    add default reply message to 'BoT Leave' for 1-on-1 chat

--- bot.php
+++ bot.php
@@ -53,6 +53,20 @@ if (!is_null($events['events'])) {
 					case "room":
 						$url = 'https://api.line.me/v2/bot/room/'.$event[source][roomId].'/leave';
 						break;
+					default:
+						// Build message to reply back
+						$messages = [
+							'type' => 'text',
+							'text' => 'ก็ไม่เกี่ยวนะ'
+						];
+
+						// Make a POST Request to Messaging API to reply to sender
+						$url = 'https://api.line.me/v2/bot/message/reply';
+						$data = [
+							'replyToken' => $replyToken,
+							'messages' => [$messages],
+						];
+						$post = json_encode($data);
 				}
 				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
